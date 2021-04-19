8700b974729a759677f2d66ee2d5b7d6695b81a4
Senjay <70802543+Sen-jay@users.noreply.github.com>
2021-04-19 17:54:48 +0900

    Add files via upload

---
+++ featured projects/stock Manager.py
@@ -0,0 +1,23 @@
+stock = 74
+item = "webcams"
+orders = 18
+
+print(f"{stock} {item} in stock")
+
+print(f"{orders} customer orders")
+
+print(f"{stock - orders} availablea")
+
+stock = 74
+item = "webcams"
+
+stock = 74
+item = "webcams"
+orders = 18
+
+print(f"{stock} {item} in stock")
+
+print(f"{orders} customer orders")
+
+print(f"{stock - orders} available")
+
